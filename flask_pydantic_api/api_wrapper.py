44f8089a4546c51ede75b62046ed40e0e6aed2db
Logi <logi.ragnarsson@ankeri.is>
2025-02-03 09:22:13 +0100

    Tighten type of return value of function being decorated

--- flask_pydantic_api/api_wrapper.py
+++ flask_pydantic_api/api_wrapper.py
@@ -2,9 +2,10 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import wraps
 from inspect import isclass
 from itertools import chain
-from typing import Any, Callable, Dict, List, Optional, ParamSpec, Tuple, Type, TypeVar, get_origin
+from typing import Any, Awaitable, Callable, Dict, List, Optional, ParamSpec, Tuple, Type, TypeVar, get_origin
 
 from flask import Response, abort, current_app, jsonify, make_response, request
+from flask.typing import ResponseReturnValue
 from pydantic import BaseModel, ConfigDict, TypeAdapter, ValidationError
 
 from .utils import (
@@ -121,6 +122,9 @@ def get_request_args(
     return args, view_kwargs, request_model
 
 
+EndpointReturnValue = ResponseReturnValue | BaseModel | Awaitable[ResponseReturnValue | BaseModel]
+
+
 # decorator that can be composed with regular Flask @blueprint.get/post/etc decorators.
 # This decorator uses type signatures to figure out the request and response pydantic models.
 def pydantic_api(
@@ -134,8 +138,8 @@ def pydantic_api(
     openapi_schema_extra: Optional[Dict[str, Any]] = None,
     model_dump_kwargs: Optional[Dict[str, Any]] = None,
     get_request_model_from_query_string: Optional[bool] = False,
-) -> Callable[[Callable[P, Any]], Callable[P, Response]]:
-    def wrap(view_func: Callable[P, Any]) -> Callable[P, Response]:
+) -> Callable[[Callable[P, EndpointReturnValue | Awaitable[EndpointReturnValue]]], Callable[P, Response]]:
+    def wrap(view_func: Callable[P, EndpointReturnValue | Awaitable[EndpointReturnValue]]) -> Callable[P, Response]:
         request_model_param_name, request_models, response_models = (
             get_annotated_models(view_func)
         )
